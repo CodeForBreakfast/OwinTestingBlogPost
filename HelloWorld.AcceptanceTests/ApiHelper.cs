080229b9c189f717cc6f1f5daee000e8c5241949
Graeme Foster <graemef@gmail.com>
2016-01-21 12:51:44 +0000

    Refactored to extract ApiHelper.

---
+++ HelloWorld.AcceptanceTests/ApiHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+
+using HelloWorld.Owin;
+
+using Microsoft.Owin.Builder;
+
+namespace HelloWorld.AcceptanceTests
+{
+    internal static class ApiHelper
+    {
+        public static HttpClient CreateTestClient()
+        {
+            return new HttpClient(CreateOwinHttpMessageHandler())
+            {
+                BaseAddress = new Uri("http://test")
+            };
+        }
+
+        private static OwinHttpMessageHandler CreateOwinHttpMessageHandler()
+        {
+            var app = new AppBuilder();
+            new Startup().Configuration(app);
+            return new OwinHttpMessageHandler(app.Build());
+        }
+    }
+}
--- HelloWorld.AcceptanceTests/FriendlyTests.cs
+++ HelloWorld.AcceptanceTests/FriendlyTests.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
 
-using HelloWorld.Owin;
-
-using Microsoft.Owin.Builder;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HelloWorld.AcceptanceTests
@@ -11,22 +7,7 @@ namespace HelloWorld.AcceptanceTests
     [TestClass]
     public class FriendlyTests
     {
-        private readonly HttpClient client;
-
-        public FriendlyTests()
-        {
-            client = new HttpClient(CreateOwinHttpMessageHandler())
-            {
-                BaseAddress = new Uri("http://test")
-            };
-        }
-
-        private static OwinHttpMessageHandler CreateOwinHttpMessageHandler()
-        {
-            var app = new AppBuilder();
-            new Startup().Configuration(app);
-            return new OwinHttpMessageHandler(app.Build());
-        }
+        private readonly HttpClient client = ApiHelper.CreateTestClient();
 
         [TestMethod]
         public void HelloSaysHowdy()
