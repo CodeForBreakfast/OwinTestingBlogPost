4a13624ff4af952ff39127a0c552fbc20f703bc5
Graeme Foster <graemef@gmail.com>
2016-01-21 09:13:21 +0000

    Added HelloWorld.Console

---
+++ HelloWorld.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+
+using HelloWorld.Owin;
+
+using Microsoft.Owin.Hosting;
+
+namespace HelloWorld.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://+:9999"))
+                while (true)
+                    Thread.Sleep(2000);
+        }
+    }
+}
