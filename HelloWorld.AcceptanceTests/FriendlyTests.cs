851f3c557a5ea5c9f18d95e223099c2c78912fb8
Graeme Foster <graemef@gmail.com>
2016-01-21 12:41:18 +0000

    Use OwinHttpMessageHandler instead of TestServer
    This allows tests to be written using a standard HttpClient rather than
    TestServer's CreateRequest() stuff.

--- HelloWorld.AcceptanceTests/FriendlyTests.cs
+++ HelloWorld.AcceptanceTests/FriendlyTests.cs
@@ -1,6 +1,9 @@
-﻿using HelloWorld.Owin;
+﻿using System;
+using System.Net.Http;
 
-using Microsoft.Owin.Testing;
+using HelloWorld.Owin;
+
+using Microsoft.Owin.Builder;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HelloWorld.AcceptanceTests
@@ -8,7 +11,22 @@ namespace HelloWorld.AcceptanceTests
     [TestClass]
     public class FriendlyTests
     {
-        private readonly TestServer server = TestServer.Create(app => new Startup().Configuration(app));
+        private readonly HttpClient client;
+
+        public FriendlyTests()
+        {
+            client = new HttpClient(CreateOwinHttpMessageHandler())
+            {
+                BaseAddress = new Uri("http://test")
+            };
+        }
+
+        private static OwinHttpMessageHandler CreateOwinHttpMessageHandler()
+        {
+            var app = new AppBuilder();
+            new Startup().Configuration(app);
+            return new OwinHttpMessageHandler(app.Build());
+        }
 
         [TestMethod]
         public void HelloSaysHowdy()
@@ -18,8 +36,7 @@ namespace HelloWorld.AcceptanceTests
 
         private string GetBody(string path)
         {
-            return server.CreateRequest(path)
-                         .GetAsync().Result
+            return client.GetAsync(path).Result
                          .Content
                          .ReadAsStringAsync().Result;
         }
