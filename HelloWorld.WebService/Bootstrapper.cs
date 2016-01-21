769bcde537a951202346fb0bff3ad537a6b8d4e0
Graeme Foster <graemef@gmail.com>
2016-01-21 11:30:38 +0000

    Disable automatic registration and module discovery

--- HelloWorld.WebService/Bootstrapper.cs
+++ HelloWorld.WebService/Bootstrapper.cs
@@ -1,8 +1,20 @@
-﻿using Nancy;
+﻿using System.Collections.Generic;
+
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
 
 namespace HelloWorld.WebService
 {
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        protected override IEnumerable<ModuleRegistration> Modules
+        {
+            get { yield return new ModuleRegistration(typeof(FriendlyModule)); }
+        }
+
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+        }
     }
 }
